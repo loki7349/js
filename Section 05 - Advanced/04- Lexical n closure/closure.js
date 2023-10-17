64d12e0bfd2fbda8a670a6d592b0db81ca8ceebf
loki7349 <lokeshargon19l@gmail.com>
2023-10-17 21:30:22 +0530

    Lexical & Closure

---
+++ Section 05 - Advanced/04- Lexical n closure/closure.js
@@ -0,0 +1,41 @@
+// function init(){
+//     let name = "mozilla";
+//     function displayName(){
+//         console.log(name);
+//     }
+//     displayName();
+// }
+// // init()
+
+// function outer(){
+//     let username = "Loki"
+//     // console.log(secret);
+
+//     function inner(){
+//         // let secret = "my123"
+//         console.log("inner", username);
+//     }
+
+//     function innerTwo(){
+//         console.log("innerTwo", username);
+//         // console.log(secret);
+//     }
+//     inner()
+//     innerTwo()
+
+// }
+// // outer()
+// // console.log("TOO OUTER", username);/
+
+
+// function makefunc(){
+//     const name = "Mozilla"
+//     function displayName(){
+//         console.log(name);
+//     }
+//     return displayName
+// }
+
+// const myFunc = makefunc()
+// // myFunc()
+
